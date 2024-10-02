eec6f4128d485ce8d8255c865a10363ce45eb9b1
Vladislav Razyhraiev <nicorvenius@gmail.com>
2024-10-02 15:45:02 +0300

    install store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,21 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { provideStore } from '@ngxs/store';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
+import { withNgxsLoggerPlugin } from '@ngxs/logger-plugin';
+
+import { environment } from "../environments/environment";
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideStore(
+      [],
+      withNgxsReduxDevtoolsPlugin({ disabled: environment.production }),
+      withNgxsLoggerPlugin({disabled: environment.production}),
+    )
+  ]
 };
